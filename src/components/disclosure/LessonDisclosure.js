a2fde7ecd572eea72d864bf2a65844a0bea27ad9
Yorack <frederic.blanc.iut2@gmail.com>
2021-04-28 19:57:28 +0200

    feat(Global): general improvement

--- src/components/disclosure/LessonDisclosure.js
+++ src/components/disclosure/LessonDisclosure.js
@@ -3,7 +3,7 @@ import { ChevronRightIcon } from "@heroicons/react/outline";
 
 const LessonDisclosure = ({ lesson, content }) => {
   return (
-    <Disclosure className="rounded-md">
+    <Disclosure className="rounded-md mb-4">
       {({ open }) => (
         <>
           <Disclosure.Button className="w-full text-left focus:outline-none rounded-md">
