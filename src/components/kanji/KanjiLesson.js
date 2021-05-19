1023925559c9859a7ed777badf6e8ec4d5572ec6
Yorack <frederic.blanc.iut2@gmail.com>
2021-05-19 20:02:40 +0200

    feat(Global): general improvement

--- src/components/kanji/KanjiLesson.js
+++ src/components/kanji/KanjiLesson.js
@@ -3,28 +3,6 @@ import KanjiLessonStarted from "./KanjiLessonStarted";
 import KanjiLessonEnd from "./KanjiLessonEnd";
 
 const KanjiLesson = () => {
-  const kanji = [
-    { character: "人", japanese: "ひと", meaning: "Personne" },
-    { character: "男", japanese: "おとこ", meaning: "Homme" },
-    { character: "女", japanese: "おんな", meaning: "Femme" },
-    { character: "子", japanese: "こ", meaning: "Enfant" },
-    { character: "日", japanese: "ひ", meaning: "Soleil" },
-    { character: "月", japanese: "つき", meaning: "Lune" },
-    { character: "時", japanese: "とき", meaning: "Temps" },
-    { character: "水", japanese: "みず", meaning: "Eau" },
-    { character: "火", japanese: "ひ", meaning: "Feu" },
-    { character: "土", japanese: "つち", meaning: "Terre" },
-    { character: "風", japanese: "かぜ", meaning: "Vent" },
-    { character: "空", japanese: "そら", meaning: "Ciel" },
-    { character: "山", japanese: "やま", meaning: "Montagne" },
-    { character: "川", japanese: "かわ", meaning: "Rivière" },
-    { character: "木", japanese: "き", meaning: "Arbre" },
-    { character: "花", japanese: "はな", meaning: "Fleur" },
-    { character: "雨", japanese: "あめ", meaning: "Pluie" },
-    { character: "雪", japanese: "ゆき", meaning: "Neige" },
-    { character: "金", japanese: "かね", meaning: "Argent" },
-    { character: "刀", japanese: "かたな", meaning: "Sabre" },
-  ];
   const [started, setStarted] = useState(false);
   const [end, setEnd] = useState(false);
   const [courseStat, setCourseStat] = useState({});
